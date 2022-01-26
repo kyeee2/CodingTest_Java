c071bc94b37cace57b2eca881677dfe0cb4120e9
kyeee2 <youngeun0208@naver.com>
2022-01-26 21:30:06 +0900

    [백준] 2004 조합0의개수

---
+++ Baekjoon/실버/2004.조합0의개수/Kyeee2.java
@@ -0,0 +1,62 @@
+package algorithm_me;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Kyeee2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String [] line = br.readLine().split(" ");
+		
+		int n = Integer.parseInt(line[0]);
+		int m = Integer.parseInt(line[1]);
+		
+		if(m > n / 2) {
+			m = n - m;
+		}
+		
+		int cntTwo = countNum(n, 2); // 2의 개수
+		cntTwo -= countNum(m, 2);
+		cntTwo -= countNum(n - m, 2);
+		
+		int cntFive = countNum(n, 5); // 5의 개수
+		cntFive -= countNum(m, 5);
+		cntFive -= countNum(n - m, 5);
+		
+		System.out.println(Math.min(cntTwo, cntFive));
+	}
+
+	private static int countNum(int n, int g) {
+//		int cnt = 0;
+		
+		/*
+		 * 시간초과 코드
+		 * */
+		/*
+		for(int i = end; i <= start; i++) {
+			int num = i;
+			
+			while(num > 1 && num % g == 0) {
+				num /= g;
+				cnt++;
+			}
+		}
+		*/
+		int cnt = 0;
+		
+		while(n > 0) {
+			cnt += n / g;
+			n /= g;
+		}
+		
+		return cnt;
+	}
+
+}
