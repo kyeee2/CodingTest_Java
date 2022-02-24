63d4a6cd455303dcb041c58e2a15e62a1293d05a
kyeee2 <youngeun0208@naver.com>
2022-02-24 23:12:52 +0900

    [백준] 15657 N과M8

---
+++ Baekjoon/SILVER3/BJ_S3_15657_N과M8.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_S3_15657_N과M8 {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	static int N, M;
+	static int [] nums, result;
+
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		
+		nums = new int [N];
+		tokens = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(tokens.nextToken());
+		}
+		Arrays.sort(nums);
+		
+		result = new int [M];
+		
+		combination(0, 0);
+		System.out.println(output);
+	}
+
+	private static void combination(int cnt, int start) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				output.append(result[i] + " ");
+			}
+			output.append('\n');
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			result[cnt] = nums[i];
+			combination(cnt + 1, i);
+		}
+	}
+
+}
