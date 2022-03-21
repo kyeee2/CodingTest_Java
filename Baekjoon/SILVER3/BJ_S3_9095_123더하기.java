6f62ca3c242a24091faf669a4f4295a4ae4f4fc9
김영은 <youngeun0208@naver.com>
2022-03-21 22:30:15 +0900

    [백준] 9095 123더하기 다른 풀이 추가

--- Baekjoon/SILVER3/BJ_S3_9095_123더하기.java
+++ Baekjoon/SILVER3/BJ_S3_9095_123더하기.java
@@ -1,5 +1,3 @@
-package bj.s3;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,22 +8,42 @@ public class BJ_S3_9095_123더하기 {
 	static StringBuilder output = new StringBuilder();
 	
 	static int T, N, cnt;
+	static int [] D = new int [11];
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		T = Integer.parseInt(br.readLine());
+		useDP();
 		for(int t = 0; t < T; t++) {
 			N = Integer.parseInt(br.readLine());
 			
+			output.append(D[N] + "\n");
+			
+			/*
 			cnt = 0;
 			findSum(0);
-			
 			output.append(cnt).append('\n');
+			*/
 		}
 		System.out.println(output);
 
 	}
 
+	// DP 사용
+	private static int useDP() {
+		D[1] = 1;
+		D[2] = 2;
+		D[3] = 4;
+		
+		for(int i = 4; i < 11; i++) {
+			D[i] = D[i - 1] + D[i - 2] + D[i - 3];
+		}
+		
+		return D[N];
+	}
+	
+	// 백트레킹 사용
+	/*
 	private static void findSum(int sum) {
 		if(sum > N) {
 			return;
@@ -41,5 +59,5 @@ public class BJ_S3_9095_123더하기 {
 		findSum(sum + 3);
 		
 	}
-
+	 */
 }
