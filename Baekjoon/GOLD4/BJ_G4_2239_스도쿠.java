e868aa5a8a4a57ba7b087133a666fd349e298798
김영은 <youngeun0208@naver.com>
2022-04-06 16:30:02 +0900

    [백준] 2239 스도쿠 풀이방법 변경

--- Baekjoon/GOLD4/BJ_G4_2239_스도쿠.java
+++ Baekjoon/GOLD4/BJ_G4_2239_스도쿠.java
@@ -12,6 +12,9 @@ public class BJ_G4_2239_스도쿠 {
 	static int N = 9;
 	static boolean flag = true;
 	static int [][] sudoku = new int [N][N];
+	static boolean [][] row = new boolean [N][10];
+	static boolean [][] col = new boolean [N][10];
+	static boolean [][] square = new boolean [N][10];
 	static List<Point> zeros = new ArrayList<>();
 	
 	static class Point {
@@ -29,6 +32,11 @@ public class BJ_G4_2239_스도쿠 {
 			for(int c = 0; c < N; c++) {
 				sudoku[r][c] = line.charAt(c) - '0';
 				if(sudoku[r][c] == 0) zeros.add(new Point(r, c));
+				else {
+					row[r][sudoku[r][c]] = true;
+					col[c][sudoku[r][c]] = true;
+					square[(r / 3) * 3 + (c / 3)][sudoku[r][c]] = true;
+				}
 			}
 		}
 		
@@ -54,36 +62,17 @@ public class BJ_G4_2239_스도쿠 {
 		int c = zeros.get(idx).c;
 		
 		for(int n = 1; n < 10; n++) {
-			sudoku[r][c] = n;
-			if(isPossible(r, c)) {
+			if(!row[r][n] && !col[c][n] && !square[(r / 3) * 3 + (c / 3)][n]) {
+				sudoku[r][c] = n;
+				row[r][n] = true;
+				col[c][n] = true;
+				square[(r / 3) * 3 + (c / 3)][n] = true;
 				dfs(idx + 1);
-			}
-			sudoku[r][c] = 0;
-		}
-	}
-
-	private static boolean isPossible(int r, int c) {
-		// 사각형
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(r - r % 3 + i == r && c - c % 3 + j == c) continue;
-				if(sudoku[r - r % 3 + i][c - c % 3 + j] == sudoku[r][c]) return false;
+				sudoku[r][c] = 0;
+				row[r][n] = false;
+				col[c][n] = false;
+				square[(r / 3) * 3 + (c / 3)][n] = false;
 			}
 		}
-		
-		// 가로
-		for(int i = 0; i < N; i++) {
-			if(c == i) continue;
-			if(sudoku[r][i] == sudoku[r][c]) return false;
-		}
-		
-		// 세로
-		for(int i = 0; i < N; i++) {
-			if(r == i) continue;
-			if(sudoku[i][c] == sudoku[r][c]) return false;
-		}
-		
-		return true;
 	}
-
 }
